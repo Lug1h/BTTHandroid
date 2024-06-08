3ccbdc605fab81258ff01d5ca88f4eb5869dd104
Viet Anh Vi Nguyen <105661295+Lug1h@users.noreply.github.com>
2024-06-08 22:58:05 +0700

    Add files via upload

---
+++ IntentCallPhone/app/src/main/java/com/example/myapplication/PhoneCall.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PhoneCall extends AppCompatActivity {
+    EditText edtPhone;
+    Button btnBack;
+    ImageButton btnPhoneCall;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_phone_call);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edtPhone = findViewById(R.id.edtPhoneNumber);
+        btnBack = findViewById(R.id.btnBack1);
+        btnPhoneCall = findViewById(R.id.btnPhoneButton);
+
+    }
+}
